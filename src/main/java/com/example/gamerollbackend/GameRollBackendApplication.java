7768686041eb5238e0d91ddf8f8f97dc205e8239
DuQer <marcinurbanski100@gmail.com>
2021-09-06 18:47:50 +0200

    Initial commit

---
+++ src/main/java/com/example/gamerollbackend/GameRollBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.gamerollbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameRollBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameRollBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gamerollbackend/GameRollBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamerollbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameRollBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
